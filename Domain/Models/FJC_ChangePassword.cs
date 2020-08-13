2fe7bdc83fac74f0c283bd0ee552e94f1686daa7
vikasbigshare <vikasbigsharep@gmail.com>
2020-08-13 19:58:12 +0530

    Update FJC_ChangePassword.cs

--- Domain/Models/FJC_ChangePassword.cs
+++ Domain/Models/FJC_ChangePassword.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; 
 
 namespace evoting.Domain.Models
 {
     public class FJC_ChangePassword
     {
+        [Required(ErroreMessage ="Enter User ID"),RegularExpression(@"^[a-zA-Z0-9]*$")]  
         public string UserID { get; set; }
+         [Required(ErroreMessage ="Enter Old Password")]
         public string encrypt_OldPassword { get; set; }
+          [Required(ErroreMessage ="Enter New Password")]
         public string encrypt_NewPassword { get; set; }
        
     }
