bbd77800771411a2cc951772910fc8c2f1dd685b
Shiv24Bigshare <shivkumar@bigshareonline.com>
2020-08-14 16:05:00 +0530

    Login changes

--- Domain/Models/FJC_LoginRequest.cs
+++ Domain/Models/FJC_LoginRequest.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; 
 
 namespace evoting.Domain.Models
 {
     public class FJC_LoginRequest
     {
-        public string system_ip { get; set; }
+        [Required(ErrorMessage ="Enter User ID"),RegularExpression(@"^[a-zA-Z0-9]*$")] 
         public string UserID { get; set; }
+
+        [Required(ErrorMessage ="Enter IP Address")]
+        public string system_ip { get; set; }
+
+        [Required(ErrorMessage ="Enter Password")]        
         public string encrypt_Password { get; set; }
     }
 }
