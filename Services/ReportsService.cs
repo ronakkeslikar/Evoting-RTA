4ecead86ff81fd58d13f3fd1519d084bb0e50495
Shiv24Bigshare <shivkumar@bigshareonline.com>
2020-12-08 18:27:22 +0530

    Pushed

--- Services/ReportsService.cs
+++ Services/ReportsService.cs
@@ -73,127 +73,131 @@ namespace evoting.Services
             
             //if(ds.Tables[0].Rows.Count>0)
             if(!ds.Tables[1].Columns.Contains("Error_Message"))
-            {
-                    //Name of File  
-                string fileName = "Scrutinizer_Evoting_Reports.xlsx";
-                using (XLWorkbook wb = new XLWorkbook())
+            {   
+                if(ds.Tables[0].Rows.Count>0)
                 {
-                                        
-                    var summarysheet=  wb.Worksheets.Add("Summary Report");
-                    //Add DataTable in Details Report worksheet  
-                    var summarysheet1=wb.Worksheets.Add(ds.Tables[0],"Details Report");
-                    //wb.Worksheets.Add(ds.Tables[0],"Details Report");
-                    summarysheet1.Style.Font.FontColor=XLColor.Black;
-                    summarysheet1.SetAutoFilter(false);
-                    summarysheet1.Tables.FirstOrDefault().Theme=XLTableTheme.None;
-                    
-                       
-                    summarysheet1.Rows(1, 1).Style.Font.Bold = true;
-                    summarysheet1.Columns().Width = 20;
-                    summarysheet1.Columns().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
-                  
-                     //Add Records for  Summary Report worksheet
-                    var col1 = summarysheet.Column("A");
-                    var colB = summarysheet.Column("B");
-                    var colC = summarysheet.Column("C");
-
-
-                    //col1.Style.Font.Bold = true;
-                    col1.Style.Font.FontColor=XLColor.Black;
-                    summarysheet.Cell(1,1).Style.Fill.BackgroundColor=XLColor.Gray;
-
-                    col1.Width = 60;
-                    colB.Width=20;
-                   summarysheet.Range("A3:B3").Style.Fill.BackgroundColor=XLColor.Gray;
-
-
-                    colC.Width=30;
-                   var secondsheetstyle= summarysheet.Cell(1, 1).SetValue("Report Generation Date and Time : " + ds.Tables[1].Rows[0]["report_generated_date"].ToString()).Style.Font.Bold=true;
- 
-                    summarysheet.Cell(3, 1).SetValue("EVSN").Style.Font.Bold=true;
-                    summarysheet.Cell(3, 2).SetValue("ISIN").Style.Font.Bold=true;
-                    summarysheet.Cell(4, 1).SetValue(ds.Tables[1].Rows[0]["EVSN"].ToString());
-                    summarysheet.Cell(4, 2).SetValue(ds.Tables[1].Rows[0]["ISIN"].ToString());
-
-                    // Voting Start Date and Time : 11-06-2020 09:00
-                    // Voting End Date and Time : 14-06-2020 17:00
-                    // Meeting Date and Start Time :15-06-2020 11:00
-                    // Voting Finalisation Date and Time: 15-06-2020 12:05
-                    summarysheet.Cell(6, 1).SetValue("Voting Start Date and Time : " + ds.Tables[1].Rows[0]["Voting Start Date and Time"].ToString() );
-                    summarysheet.Cell(7, 1).SetValue("Voting End Date and Time : " + ds.Tables[1].Rows[0]["Voting End Date and Time"].ToString());
-                    summarysheet.Cell(8, 1).SetValue("Meeting Date and Start Time : " +ds.Tables[1].Rows[0]["Meeting Date and Start Time"].ToString());
-                    summarysheet.Cell(9, 1).SetValue("Voting Finalisation Date and Time: " + ds.Tables[1].Rows[0]["Voting Finalisation Date and Time"].ToString());
-
-                    //Row start from ROW 11
-                    int rowint=11;
-                     summarysheet.Cell(rowint, 1).SetValue("Res. No.").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 2).SetValue("Yes Count").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 3).SetValue("Yes (%)").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 4).SetValue("No Count").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 5).SetValue("No (%)").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 6).SetValue("TotalCount").Style.Font.Bold=true;
-                     summarysheet.Cell(rowint, 7).SetValue("Total").Style.Font.Bold=true;
-                     summarysheet.Range("A11:G11").Style.Fill.BackgroundColor=XLColor.Gray;
-                    //summarysheet.Cell(ds.Tables[2].Rows.Count, ds.Tables[2].Columns.Count).SetValue(ds.Tables[2]);
-               int a=12;
-                foreach(DataRow dr in ds.Tables[2].Rows)
-                {
-                    //Res. No.	Yes Count	Yes (%)	No Count	No (%)	TotalCount	Total
-
-                     summarysheet.Cell(a,1).SetValue(dr["Res. No."].ToString());
-                     summarysheet.Cell(a,2).SetValue(dr["Yes Count"].ToString());
-                     summarysheet.Cell(a,3).SetValue(dr["Yes (%)"].ToString());
-                     summarysheet.Cell(a,4).SetValue(dr["No Count"].ToString());
-                     summarysheet.Cell(a,5).SetValue(dr["No (%)"].ToString());
-                     summarysheet.Cell(a,6).SetValue(dr["TotalCount"].ToString());
-                     summarysheet.Cell(a,7).SetValue(dr["Total"].ToString());
-
-                    a++;
-                    
-                }
-
-
-                    using (MemoryStream memoryStream = new MemoryStream())
+                        //Name of File  
+                    string fileName = "Scrutinizer_Evoting_Reports.xlsx";
+                    using (XLWorkbook wb = new XLWorkbook())
                     {
-                        wb.SaveAs(memoryStream);
-                        //Return xlsx Excel File  
-                        // File(memoryStream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
-                        //Save To Folder
-                        byte[] bytes = memoryStream.ToArray();
-                        memoryStream.Close();
+                                            
+                        var summarysheet=  wb.Worksheets.Add("Summary Report");
+                        //Add DataTable in Details Report worksheet  
+                        var summarysheet1=wb.Worksheets.Add(ds.Tables[0],"Details Report");
+                        //wb.Worksheets.Add(ds.Tables[0],"Details Report");
+                        summarysheet1.Style.Font.FontColor=XLColor.Black;
+                        summarysheet1.SetAutoFilter(false);
+                        summarysheet1.Tables.FirstOrDefault().Theme=XLTableTheme.None;
+                        
+                        
+                        summarysheet1.Rows(1, 1).Style.Font.Bold = true;
+                        summarysheet1.Columns().Width = 20;
+                        summarysheet1.Columns().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
+                    
+                        //Add Records for  Summary Report worksheet
+                        var col1 = summarysheet.Column("A");
+                        var colB = summarysheet.Column("B");
+                        var colC = summarysheet.Column("C");
 
-                        //convert byte to pdf and save
-                        string actPath = FolderPaths.Scrutinizer.Scrutinizer_Reports();//@"C:\evoting\Agreement\";
 
-                        string pdffilename = System.DateTime.Now.ToString("yyyyMMdd-HHmmssfff") + fileName;
-                        System.IO.File.WriteAllBytes(Path.Combine(actPath , pdffilename), bytes);
-                        string filePath = Path.Combine(actPath , pdffilename);
+                        //col1.Style.Font.Bold = true;
+                        col1.Style.Font.FontColor=XLColor.Black;
+                        summarysheet.Cell(1,1).Style.Fill.BackgroundColor=XLColor.Gray;
 
-                        //Saving PDF to Folder and database
-                        if(filePath.Length >0 && filePath != null)
-                        {
-                            Dictionary<string, object> dictfileDnld = new Dictionary<string, object>();               
-                        dictfileDnld.Add("@File_Name", pdffilename);
-                        dictfileDnld.Add("@File_Path", filePath);
-                        dictfileDnld.Add("@token", Token);
-                        dictfileDnld.Add("@event_id",event_id);
-                        dictfileDnld.Add("@flag",0);
+                        col1.Width = 60;
+                        colB.Width=20;
+                    summarysheet.Range("A3:B3").Style.Fill.BackgroundColor=XLColor.Gray;
 
-                        DataSet ds2 = new DataSet();
-                        ds2 = await AppDBCalls.GetDataSet("Evote_SpExcelFile_Download", dictfileDnld);
-                        return Reformatter.Validate_DataTable(ds2.Tables[0]);
-                        }
-                
-                        else
-                        {
-                            throw new CustomException.InvalidFileRejected();
-                        } 
+
+                        colC.Width=30;
+                    var secondsheetstyle= summarysheet.Cell(1, 1).SetValue("Report Generation Date and Time : " + ds.Tables[1].Rows[0]["report_generated_date"].ToString()).Style.Font.Bold=true;
+    
+                        summarysheet.Cell(3, 1).SetValue("EVSN").Style.Font.Bold=true;
+                        summarysheet.Cell(3, 2).SetValue("ISIN").Style.Font.Bold=true;
+                        summarysheet.Cell(4, 1).SetValue(ds.Tables[1].Rows[0]["EVSN"].ToString());
+                        summarysheet.Cell(4, 2).SetValue(ds.Tables[1].Rows[0]["ISIN"].ToString());
+
+                        // Voting Start Date and Time : 11-06-2020 09:00
+                        // Voting End Date and Time : 14-06-2020 17:00
+                        // Meeting Date and Start Time :15-06-2020 11:00
+                        // Voting Finalisation Date and Time: 15-06-2020 12:05
+                        summarysheet.Cell(6, 1).SetValue("Voting Start Date and Time : " + ds.Tables[1].Rows[0]["Voting Start Date and Time"].ToString() );
+                        summarysheet.Cell(7, 1).SetValue("Voting End Date and Time : " + ds.Tables[1].Rows[0]["Voting End Date and Time"].ToString());
+                        summarysheet.Cell(8, 1).SetValue("Meeting Date and Start Time : " +ds.Tables[1].Rows[0]["Meeting Date and Start Time"].ToString());
+                        summarysheet.Cell(9, 1).SetValue("Voting Finalisation Date and Time: " + ds.Tables[1].Rows[0]["Voting Finalisation Date and Time"].ToString());
+
+                        //Row start from ROW 11
+                        int rowint=11;
+                        summarysheet.Cell(rowint, 1).SetValue("Res. No.").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 2).SetValue("Yes Count").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 3).SetValue("Yes (%)").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 4).SetValue("No Count").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 5).SetValue("No (%)").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 6).SetValue("TotalCount").Style.Font.Bold=true;
+                        summarysheet.Cell(rowint, 7).SetValue("Total").Style.Font.Bold=true;
+                        summarysheet.Range("A11:G11").Style.Fill.BackgroundColor=XLColor.Gray;
+                        //summarysheet.Cell(ds.Tables[2].Rows.Count, ds.Tables[2].Columns.Count).SetValue(ds.Tables[2]);
+                int a=12;
+                    foreach(DataRow dr in ds.Tables[2].Rows)
+                    {
+                        //Res. No.	Yes Count	Yes (%)	No Count	No (%)	TotalCount	Total
+
+                        summarysheet.Cell(a,1).SetValue(dr["Res. No."].ToString());
+                        summarysheet.Cell(a,2).SetValue(dr["Yes Count"].ToString());
+                        summarysheet.Cell(a,3).SetValue(dr["Yes (%)"].ToString());
+                        summarysheet.Cell(a,4).SetValue(dr["No Count"].ToString());
+                        summarysheet.Cell(a,5).SetValue(dr["No (%)"].ToString());
+                        summarysheet.Cell(a,6).SetValue(dr["TotalCount"].ToString());
+                        summarysheet.Cell(a,7).SetValue(dr["Total"].ToString());
+
+                        a++;
+                        
                     }
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            wb.SaveAs(memoryStream);
+                            //Return xlsx Excel File  
+                            // File(memoryStream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                            //Save To Folder
+                            byte[] bytes = memoryStream.ToArray();
+                            memoryStream.Close();
+
+                            //convert byte to pdf and save
+                            string actPath = FolderPaths.Scrutinizer.Scrutinizer_Reports();//@"C:\evoting\Agreement\";
+
+                            string pdffilename = System.DateTime.Now.ToString("yyyyMMdd-HHmmssfff") + fileName;
+                            System.IO.File.WriteAllBytes(Path.Combine(actPath , pdffilename), bytes);
+                            string filePath = Path.Combine(actPath , pdffilename);
+
+                            //Saving PDF to Folder and database
+                            if(filePath.Length >0 && filePath != null)
+                            {
+                                Dictionary<string, object> dictfileDnld = new Dictionary<string, object>();               
+                            dictfileDnld.Add("@File_Name", pdffilename);
+                            dictfileDnld.Add("@File_Path", filePath);
+                            dictfileDnld.Add("@token", Token);
+                            dictfileDnld.Add("@event_id",event_id);
+                            dictfileDnld.Add("@flag",0);
+
+                            DataSet ds2 = new DataSet();
+                            ds2 = await AppDBCalls.GetDataSet("Evote_SpExcelFile_Download", dictfileDnld);
+                            return Reformatter.Validate_DataTable(ds2.Tables[0]);
+                            }
+                    
+                            else
+                            {
+                                throw new CustomException.InvalidFileRejected();
+                            } 
+                        }
+                    }
+                }
+                else
+                {
+                    throw new CustomException.InvalidFileRejected();
                 }
             }
              else
-            {
-                
+            {              
         
                 string filenamewithdatetime;
                 //File name with time stamp and Event no 
@@ -240,11 +244,7 @@ namespace evoting.Services
                     
                 }
                 return ds1.Tables[0];
-            }
-            // else
-            // {
-            //     throw new CustomException.InvalidActivity();
-            // }
+            }            
             
         }
         public async Task<DataSet> getExcelData(string Token,int event_id)
