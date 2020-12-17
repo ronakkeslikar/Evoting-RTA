b0b9501ca7581c90b3c916ab6d8ca0c80511c015
Shiv24Bigshare <shivkumar@bigshareonline.com>
2020-12-17 12:50:43 +0530

    Registration regular expression error msg added

--- Domain/Models/FJC_Registration.cs
+++ Domain/Models/FJC_Registration.cs
@@ -12,20 +12,20 @@ namespace evoting.Domain.Models
         public int aud_id { get; set;}
         public int reg_type_id { get; set;}
 
-        [Required (ErrorMessage ="Enter Entity Name") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$")] 
+        [Required (ErrorMessage ="Enter Entity Name") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$",ErrorMessage = "Please enter valid Entity Name")] 
         public string name { get; set;}  
 
         //[Required (ErrorMessage ="Enter Reg. No.") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$")] 
         public string reg_no { get; set;}
                 
-        [Required (ErrorMessage ="Enter Registered Office Address") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$")] 
+        [Required (ErrorMessage ="Enter Registered Office Address") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$",ErrorMessage = "Please enter valid Registered Office Address")] 
         public string reg_add1 { get; set;}
          
         public string reg_add2 { get; set;}
         public string reg_add3 { get; set;}
         [Required (ErrorMessage ="Enter City")] 
         public string reg_city { get; set;}
-         [Required (ErrorMessage ="Enter Pincode"),RegularExpression(@"^[0-9]*$")] 
+         [Required (ErrorMessage ="Enter Pincode"),RegularExpression(@"^[0-9]*$",ErrorMessage = "Please enter valid Pincode")] 
          [MinLength(6, ErrorMessage = "Pincode Cannot be less than 6 digit")]  
          [MaxLength(6, ErrorMessage = "Pincode Cannot be more than 6 digit")]  
 
@@ -34,13 +34,13 @@ namespace evoting.Domain.Models
         public int reg_state_id { get; set;}
            [Required (ErrorMessage ="Enter Country")] 
         public int reg_country_id { get; set;}       
-        [Required (ErrorMessage ="Enter Correspondence Address") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$")] 
+        [Required (ErrorMessage ="Enter Correspondence Address") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$",ErrorMessage = "Please enter valid Correspondence Address")] 
         public string corres_add1 { get; set;}
         public string corres_add2 { get; set;}
         public string corres_add3 { get; set;}
          [Required (ErrorMessage ="Enter City")]
         public string corres_city { get; set;}
-         [Required (ErrorMessage ="Enter Pincode"),RegularExpression(@"^[0-9]*$")] 
+         [Required (ErrorMessage ="Enter Pincode"),RegularExpression(@"^[0-9]*$",ErrorMessage = "Please enter valid Pincode")] 
          [MinLength(6, ErrorMessage = "Pincode Cannot be less than 6 digit")]  
          [MaxLength(6, ErrorMessage = "Pincode Cannot be more than 6 digit")] 
         public string corres_pincode { get; set;}
@@ -49,18 +49,18 @@ namespace evoting.Domain.Models
              [Required (ErrorMessage ="Enter Country")] 
         public int corres_country_id { get; set;}       
         public string pcs_no { get; set;}
-         [Required (ErrorMessage ="Enter NAME") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$")] 
+         [Required (ErrorMessage ="Enter NAME") ,RegularExpression(@"^[a-zA-Z0-9 -,_]*$",ErrorMessage = "Please enter valid NAME")] 
         public string cs_name { get; set;}
-         [Required (ErrorMessage ="Enter EMAIL_ID") ,RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+         [Required (ErrorMessage ="Enter EMAIL_ID") ,RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$",ErrorMessage = "Please enter valid EMAIL ID")]
         public string cs_email_id { get; set;}
         public string cs_alt_email_id { get; set;}
-         [Required (ErrorMessage ="Enter Telephone No.") ,RegularExpression(@"^[0-9]*$")] 
+         [Required (ErrorMessage ="Enter Telephone No.") ,RegularExpression(@"^[0-9]*$",ErrorMessage = "Please enter valid Telephone No.")] 
         public string cs_tel_no { get; set;}
         public string cs_fax_no { get; set;}
-        [Required (ErrorMessage ="Enter Mobile No.") ,RegularExpression(@"^[0-9]*$")] 
+        [Required (ErrorMessage ="Enter Mobile No.") ,RegularExpression(@"^[0-9]*$",ErrorMessage = "Please enter valid Mobile No.")] 
         public string cs_mobile_no { get; set;}
          
-     [Required (ErrorMessage ="Enter PAN ID") ,RegularExpression(@"^[a-zA-Z0-9]*$")] 
+     [Required (ErrorMessage ="Enter PAN ID") ,RegularExpression(@"^[a-zA-Z0-9]*$",ErrorMessage = "Please enter valid PAN ID")] 
         public string panid { get; set;}
         public string alt_mob_num{get;set;}//change from decimal to string 
           public int rta_id{get;set;}
